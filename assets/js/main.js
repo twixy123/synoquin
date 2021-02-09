aad3555f26a6693cf0680e6c003e1344078c7917
Ruslan <luckytwixy321@gmail.com>
2021-02-09 14:56:17 +0500

    Добавил адаптацию

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,26 +21,45 @@ const landing = document.getElementById('landing')
 
 if (landing) {
   setHeigthTrustCards()
-  window.addEventListener('load', ()=>{
+  window.addEventListener('load', () => {
     setHeigthTrustCards()
   })
 }
 
 function setHeigthTrustCards() {
   const trustCards = document.querySelectorAll('.people_trust__card')
+
   trustCards.forEach((tc, i, arr) => {
     tc.classList.add('with_line')
     const heightArr = []
-    arr.forEach(h => heightArr.push(h.clientHeight))
-    const height = Math.max.apply(null, heightArr)
-    tc.style = `min-height: ${height}px !important;`
+    if (document.body.clientWidth > 540) {
+      arr.forEach(h => heightArr.push(h.clientHeight))
+      const height = Math.max.apply(null, heightArr)
+      tc.style = `min-height: ${height}px !important;`
+    }
   })
-  if (trustCards.length > 4) {
+
+  if (document.body.clientWidth < 820) {
     trustCards.forEach((tc, tci) => {
-      if ((tci + 1) % 3 == 0) {
+      if ((tci + 1) % 2 == 0) {
         tc.classList.remove('with_line')
       }
     })
   }
-  trustCards[trustCards.length - 1].classList.remove(('with_line'))
+
+  if (trustCards.length > 4) {
+    trustCards.forEach((tc, tci) => {
+      if (document.body.clientWidth > 820 && (tci + 1) % 3 == 0) {
+        tc.classList.remove('with_line')
+      }
+    })
+  } else{
+    trustCards.forEach((tc, tci) => {
+      if (document.body.clientWidth > 820) {
+        tc.classList.add('four_element')
+      }
+    })
+  }
+  if (document.body.clientWidth > 767)
+    trustCards[trustCards.length - 1].classList.remove(('with_line'))
 }
